4bca6da561663db66cdc892655e0ed79ba894753
dariusua <119784522+dariusua@users.noreply.github.com>
2025-04-26 19:13:22 +0300

    Update game-min_custom.js

--- game-min_custom.js
+++ game-min_custom.js
@@ -966,7 +966,7 @@
     this.ab = [];
     this.Ar = new R(["Time is", "Up!"], 16777215);
     this.Gq = new R(["Red is", "Victorious!"], 15035990);
-    this.Fq = new R(["Red", "Scores!"], 15035990);
+    this.Fq = new R(["Red", "Scores! YAAAAY!"], 15035990);
     this.Cn = new R(["Blue is", "Victorious!"], 625603);
     this.Bn = new R(["Blue", "Scores!"], 625603);
     this.eq = new R(["Game", "Paused"], 16777215);
